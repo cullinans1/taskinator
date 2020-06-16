42d7732d9315ca02f9434cb75a761867a71fb0f4
cullinans1  <sammy.cullinan@gmail.com>
2020-06-16 12:09:21 -0700

    added reset to form

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,6 +5,11 @@ var taskFormHandler = function(event) {
     event.preventDefault();
     var taskNameInput = document.querySelector("input[name='task-name']").value;
     var taskTypeInput = document.querySelector("select[name='task-type']").value;
+    if (!taskNameInput || !taskTypeInput) {
+        alert("You need to fill out the task form completely!");
+        return false;
+    }
+    formEl.reset();
     //put data into an objet
     var taskDataObj = {
         name: taskNameInput,
